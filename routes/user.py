35e7ea57443bd1502dde0c509b862f78a915b6cd
dany guancha <dany.1701627413@ucaldas.edu.co>
2023-11-09 01:51:00 -0500

    crud de LibroFisico y prestamo

--- routes/user.py
+++ routes/user.py
@@ -9,7 +9,7 @@ from controllers.user import create_user, exist_user, all_users, delete_users
 router = APIRouter()
 
 #nuevo usuario
-@router.post("/")
+@router.post("/", tags=["User"], description="Create new user")
 def create_new_user(usuario: UserCreate, db: Session = Depends(get_db)):
     exist = exist_user(usuario.correo, db)
     if exist:
@@ -19,9 +19,9 @@ def create_new_user(usuario: UserCreate, db: Session = Depends(get_db)):
 
 
 #obtener usuario por correo
-@router.get("/{correo}")
-def get_user(id: str, db: Session = Depends(get_db)):
-    exist = exist_user(id, db)
+@router.get("/{correo}", tags=["User"], description="Get user by email")
+def get_user(correo: str, db: Session = Depends(get_db)):
+    exist = exist_user(correo, db)
     if not exist:
         return {"message": "User not exist"}
     
@@ -34,7 +34,7 @@ def get_all_users(db: Session = Depends(get_db)):
     return all_users(db)
 
 #eliminar usuarios por id
-@router.delete("/delete/{id}")
+@router.delete("/delete/{id}", tags=["User"], description="Delete user by id")
 def delete_user(id: str, db: Session = Depends(get_db)):
     userDeleted = delete_users(id, db)
     if not userDeleted:
